05be780ef694a8db21c028255ed0109474a40794
Misato Yamamoto <misa_to_1213@hotmail.com>
2020-08-20 22:29:33 -0600

    20200820

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+
+    $(document).ready(function(){
+      $(window).scroll(function() { // check if scroll event happened
+        if ($(document).scrollTop() > 50) { // check if user scrolled more than 50 from top of the browser window
+          $(".navbar-fixed-top").css("background-color", "#f8f8f8"); // if yes, then change the color of class "navbar-fixed-top" to white (#f8f8f8)
+        } else {
+          $(".navbar-fixed-top").css("background-color", "transparent"); // if not, change it back to transparent
+        }
+      });
+    });
+
+    $(document).ready(function() {
+        $(window).scroll(function() {
+          if ($(this).scrollTop() > 0) {
+            $('header').css('background-color', '#FFFFFF');
+          } else {
+            $('header').css('background', 'none');
+          }
+        });
+      });
+// var vid = document.getElementById("myVideo");
+
+// function getPlaySpeed() { 
+//     alert(vid.playbackRate);
+// } 
+
+// function setPlaySpeed() { 
+//     vid.playbackRate = 0.5;
+// } 
